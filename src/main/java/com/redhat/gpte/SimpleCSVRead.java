b970ef9bf4b71f247e82ceb11f1bf03834131b1a
prakrish <prakrish@localhost.localdomain>
2019-01-03 01:35:54 +0530

    Simple Data Analytics Application

--- src/main/java/com/redhat/gpte/SimpleCSVRead.java
+++ src/main/java/com/redhat/gpte/SimpleCSVRead.java
@@ -1,6 +1,6 @@
 /**
  * 
- */
+ *//*
 package com.redhat.gpte;
 
 import org.apache.spark.sql.AnalysisException;
@@ -8,10 +8,10 @@ import org.apache.spark.sql.Dataset;
 import org.apache.spark.sql.Row;
 import org.apache.spark.sql.SparkSession;
 
-/**
+*//**
  * @author prakrish
  *
- */
+ *//*
 public class SimpleCSVRead {
 	static SparkSession spark = SparkSession.builder()
 			.appName("SimpleCSVRead")
@@ -21,14 +21,16 @@ public class SimpleCSVRead {
 
 	
 	public static void main(String[] args) throws AnalysisException {
-		String csvFile = "src/main/resources/data/moviedata.csv";
+		String csvFile = "/home/prakrish/spark-simple/src/main/resources/moviedata.csv";
 		Dataset<Row> df = spark.read().format("com.databricks.spark.csv").option("header", "true").load(csvFile);
 					
 		df.createGlobalTempView("movie");
 		df.schema();
 		df.printSchema();
 		df.show();
+		df.registerTempTable("MovieTable");
 		
 		
 	}
 }
+*/
